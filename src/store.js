7016d39a85418aed6bd00e93a605f0ef6946994a
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-23 11:54:02 +0100

    add: commento

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-
-
+    //qui metteremo un array vuoto che verra poi riempito di dati forniti dalla chiamata api nel componente AppApi
 })
