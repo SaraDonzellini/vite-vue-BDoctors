8596610b54cbfa5b12189ad3623a5be4b74342d2
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-23 11:53:41 +0100

    create: router.js

--- src/router.js
+++ src/router.js
@@ -1,15 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import Doctors from './pages/PageDoctors.vue';
+import Home from './pages/PageHome.vue';
+
+
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+
+    {
+      path: '/doctors',
+      name: 'doctors',
+      component: Doctors
     },
 
   ]
+
 });
-
-
 
 export { router };
