613a4f59507c3e068127afa1e9a6a7be5c4d5f52
GiadaGallitto <gallitto.giada@gmail.com>
2023-03-23 16:30:28 +0100

    Add jumbotron to home page

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -10,15 +10,29 @@ export default {
 </script>
 
 <template>
+  <section class="jumbotron d-flex align-items-center w-100">
     <div class="container">
       <h1>
-        Home
+        Benvenuta/o in Bdoctors!
       </h1>
+      <h5 class="mb-4">
+        Cerca tra i nostri dottori per trovare il tuo specialista
+      </h5>
+      <a href="" class="btn btn-info">Tutti i nostri dottori</a>
     </div>  
+  </section>
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
 @use '../styles/partials/variables' as * ;
+
+.jumbotron{
+  height: 600px;
+  background-image: url('../../public/imgs/jumbo-home.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position-y: 25%;
+}
 
 </style>
