727b4529af518639d65fa42ba2eb95d61450ead3
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-23 11:52:21 +0100

    add: components in appVue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+    components:{
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
+    data(){
+    return {   
+    }
+  },
+
+  methods:{
+},
+
+created(){
+  
+}
+  
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <AppHeader/>
+  </header>
+  <main>
+     <AppMain/>
+  </main>
+  <footer>
+     <AppFooter/>
+  </footer>
 </template>
 
 <style lang="scss">
