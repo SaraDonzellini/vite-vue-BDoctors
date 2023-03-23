4add3dfd323c68401249b4d4c454d67c944576d0
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-23 11:53:07 +0100

    create: components in pages folder

---
+++ src/pages/PageDoctors.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+  name: 'PageDoctors',
+  components: {
+    
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="text-dark">
+      <h1>DOCTORS</h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables' as * ;
+</style>
+
+
+
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+  name: 'PageHome',
+  components: {
+    
+  }
+}
+
+</script>
+
+<template>
+    <div class="container">
+      <h1>
+        Home
+      </h1>
+    </div>  
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables' as * ;
+
+</style>
