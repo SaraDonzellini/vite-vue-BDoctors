263bfa43c54735c2925214204fb0043f3eaa62f8
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-24 17:56:29 +0100

    Edit 'PageDoctors.vue' file

--- src/pages/PageDoctors.vue
+++ src/pages/PageDoctors.vue
@@ -1,13 +1,19 @@
 <script>
 import DoctorCard from '../components/DoctorCard.vue';
+
+//importiamo la componente per la chiamata axios, con risposta l'elenco delle specializzazioni
+import SelectSpecializations from '../components/SelectSpecializations.vue';
+
 import axios from 'axios';
-// import { store } from '../store';
 
 export default {
   name: 'PageDoctors',
+
   components: {
     DoctorCard,
+    SelectSpecializations
   },
+
   data() {
     return {
       doctors: [],
@@ -16,26 +22,35 @@ export default {
   },
   methods: {
     getDoctors() {
-        axios.get('http://127.0.0.1:8000/api/doctors', {
-            params: {
+      axios.get('http://127.0.0.1:8000/api/doctors', {
+        params: {
 
-            }
-        })
-            .then((response) => {
-            this.doctors = (response.data.response.data);
-            })
-            .catch(function (error) {
-            console.warn(error);
-            });
         }
+      })
+        .then((response) => {
+          this.doctors = (response.data.response.data);
+        })
+        .catch(function (error) {
+          console.warn(error);
+        });
+    }
   },
   created() {
-        this.getDoctors();
-    },
+    this.getDoctors();
+  },
 }
 </script>
 
 <template>
+  <section class="search-doctors">
+    <div class="row">
+      <div class="col-2 m-auto my-2">
+        <SelectSpecializations />
+      </div>
+
+    </div>
+  </section>
+
   <DoctorCard v-for="doctor in doctors" :doctor="doctor" />
 </template>
 
