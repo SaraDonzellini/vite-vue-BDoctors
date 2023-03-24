32b8a19146a9be25ace437f25e8f9b5829e453c7
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-24 11:27:56 +0100

    create: pages for specialization selected

---
+++ src/pages/PageSelectDoct.vue
@@ -0,0 +1,40 @@
+<script>
+import { store } from '../store';
+import AppHeader from '../components/AppHeader.vue';
+import PageSpecDoctor from '../pages/PageSpecDoctor.vue';
+import CallsApi from '../components/CallsApi.vue';
+
+export default {
+  name: 'PageHome',
+  components: {
+    AppHeader,
+    PageSpecDoctor,
+    CallsApi,
+
+  },
+    data() {
+        return {
+            store,
+        }
+    },
+    
+}
+
+</script>
+
+<template>
+    <div class="container mt-4">
+        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4">
+          <AppHeader @changeType="doctorsSelect"/>
+        </div>
+        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4 text-dark">
+          <PageSpecDoctor/>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables' as *;
+
+</style>
---
+++ src/pages/PageSpecDoctor.vue
@@ -0,0 +1,36 @@
+<script>
+
+import { store } from '../store.js'
+export default{
+    name:'cardComp',
+    data(){
+        return {
+            store,
+        }   
+    }
+}
+
+</script>
+
+<template>
+    <div class="col" v-for="element in store.doctorsList">
+        <div class="card ">
+            <div class="card-body">
+                <h5 class="card-title"> {{ element.specialization.title }}</h5>
+                <p class="card-text">{{ element.specialization.color }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<style lang="scss">
+.card-body{
+    height:100px;
+    h5{
+        font-size: 1rem;
+    }
+}
+
+</style>
