9500c32a8333532c5ed20a3a48fbb878cd55774d
SaraDonzellini <saradonzie@gmail.com>
2023-03-24 16:44:32 +0100

    implementing singlepagedash

--- src/pages/single-page-partials/SinglePageDash.vue
+++ src/pages/single-page-partials/SinglePageDash.vue
@@ -2,40 +2,75 @@
 
 
 export default {
-  name: SinglePageDash,
+  name: 'SinglePageDash',
   data() {
     return {
-      
+
     }
   },
   props: {
-        'doctor': {
-            type: Object,
-            required: true,
-        },
-    }
+    'doctor': {
+      type: Object,
+      required: true,
+    },
+  },
+
 }
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg bg-light">
+  <nav class="navbar navbar-expand-lg my_navbar">
     <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-          <a class="nav-link" href="#">Features</a>
-          <a class="nav-link" href="#">Pricing</a>
-          <a class="nav-link disabled">Disabled</a>
-        </div>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav d-flex justify-content-between">
+          <li class="nav-item">
+            <a class="nav-link my_navbar" href="#">Informazioni Dottore</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link my_navbar" href="#">Recensioni</a>
+          </li>
+
+        </ul>
       </div>
     </div>
   </nav>
+  <div class="container">
+    <div class="row">
+      <div class="col-5 m-5">
+
+        Bio: {{doctor.bio}}
+
+      </div>
+      <div class="col-5 m-5">
+        Curriculum: {{doctor.curriculum}}
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-5 m-5">
+
+        Recensioni: {{reviews}}
+
+      </div>
+      <div class="col-5 m-5">
+        Curriculum: {{doctor.curriculum}}
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+@use '../../styles/general.scss' as *;
+@use '../../styles/partials/variables' as * ;
 
+
+.my_navbar {
+  background-color: $primary-bg-color;
+  color: $primary-text-color;
+}
 </style>
