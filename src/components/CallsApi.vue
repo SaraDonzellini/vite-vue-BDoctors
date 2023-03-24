3b32f7d157eb5c97db554a0b0f6f6b19256ef64b
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-24 11:25:26 +0100

    add: call api in comp callsApi for specizialization

--- src/components/CallsApi.vue
+++ src/components/CallsApi.vue
@@ -14,32 +14,55 @@ export default {
       urlAddress: 'http://127.0.0.1:8000/',
     }
   },
-
+ 
+  //chiamata per tutti i dottori
   methods: {
-
-    getDoctors() {
+    getDoctor() {
       axios.get(this.urlAddress + 'api/doctors', {
         params: {
-
+           
         }
       })
 
-        .then((response) => {
-          console.log(response.data.response.data);
-          this.store.doctorsList = response.data.response.data;
-        })
+      .then((response) => {
+        console.log(response.data.response.data);
+        this.store.doctorsList = response.data.response.data;
+      })
 
-        .catch(function (error) {
-          console.log(error);
-        })
+      .catch(function (error) {
+        console.log(error);
+      })
 
     }
   },
-  created() {
-    this.getDoctors();
+
+  //chiamata per i dottori selezionati
+  getDoctorSelect(specialization) {
+    axios.get(this.urlAddress, {
+      params: {
+        specialization: specialization
+      }
+    })
+
+    .then((response) => {
+      console.log(response.data.response.data);
+      this.store.doctorsSelect = response.data.response.data;
+    })
+
+    .catch(function (error) {
+      console.log(error);
+    })
+
   },
+  
+  
+  created() {
+    this.getDoctor();
+    this.getDoctorSelect()
+  }
 
 }
+
 </script>
 
 <template></template>
