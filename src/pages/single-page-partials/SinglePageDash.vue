793499e41db5d5dc0245a5c50a275acc09f681fa
SaraDonzellini <saradonzie@gmail.com>
2023-03-27 13:38:08 +0200

    moved reviewform before reviews

--- src/pages/single-page-partials/SinglePageDash.vue
+++ src/pages/single-page-partials/SinglePageDash.vue
@@ -22,9 +22,9 @@ export default {
 
   },
   methods: {
+    
   },
   created() {
-
   },
 
 
@@ -41,12 +41,8 @@ export default {
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav d-flex justify-content-between">
           <li class="nav-item">
-          <a class="nav-link my_navbar" href="#address">Informazioni Dottore</a>
-        </li>
-          <!-- <li class="nav-item">
-                              <a class="nav-link my_navbar" href="#recensioni">Recensioni</a>
-                            </li> -->
-
+            <a class="nav-link my_navbar" href="#address">Informazioni Dottore</a>
+          </li>
         </ul>
       </div>
     </div>
@@ -54,8 +50,10 @@ export default {
   <div class="container card shadow-lg">
     <div class="row">
       <div class="col-12 col-md-5 m-2">
-        <h5>Indirizzo:</h5><p> {{ doctor.address }}</p>
-        <h5>Telefono:</h5><p> {{ doctor.phone }}</p>
+        <h5>Indirizzo:</h5>
+        <p> {{ doctor.address }}</p>
+        <h5>Telefono:</h5>
+        <p> {{ doctor.phone }}</p>
         <h5>Biografia:</h5>
         <p>
           {{ doctor.bio }}
@@ -64,48 +62,12 @@ export default {
       </div>
       <div class="col-12 col-md-5 m-2">
         <h5>
-          Curriculum: 
+          Curriculum:
         </h5>
         <p>
           {{ doctor.curriculum }}
         </p>
       </div>
-    </div>
-  </div>
-  <nav class="navbar navbar-expand-lg my_navbar">
-    <div class="container-fluid">
-      <button class="navbar-toggler py-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav d-flex justify-content-between">
-          <!-- <li class="nav-item">
-                              <a class="nav-link my_navbar" href="#address">Informazioni Dottore</a>
-                            </li> -->
-          <li class="nav-item">
-            <a class="nav-link my_navbar" href="#recensioni">Recensioni</a>
-          </li>
-
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <div class="container-fluid card shadow-lg">
-    <div class="row">
-      <div class="col-12 m-2">
-        <ul>
-          <li v-for="(reviewEl, index) in review" :key="index">
-            <h5>Voto: {{ reviewEl.vote }}
-            </h5>
-            <h6>{{ reviewEl.name }} {{ reviewEl.surname }}</h6>
-            <p>{{ reviewEl.text }}</p>
-
-          </li>
-
-        </ul>
-
-      </div>
-
     </div>
   </div>
   <section id="recensioni">
@@ -137,8 +99,8 @@ export default {
                 <h6>Cognome:</h6>
               </label>
               <input class="d-block form-control" name="surname" type="text" id="surname">
-            </div>
-            <div class="vote col-12 col-md-3  px-2">
+          </div>
+          <div class="vote col-12 col-md-3  px-2">
               <label for="vote">
                 <h6>Voto:</h6>
               </label>
@@ -162,6 +124,39 @@ export default {
       </div>
     </div>
   </section>
+  <nav class="navbar navbar-expand-lg my_navbar">
+    <div class="container-fluid">
+      <button class="navbar-toggler py-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav d-flex justify-content-between">
+          <li class="nav-item">
+            <a class="nav-link my_navbar" href="#recensioni">Recensioni</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container-fluid card shadow-lg">
+    <div class="row">
+      <div class="col-12 m-2">
+        <ul>
+          <li v-for="(reviewEl, index) in review" :key="index">
+            <h5>Voto: {{ reviewEl.vote }}
+            </h5>
+            <h6>{{ reviewEl.name }} {{ reviewEl.surname }}</h6>
+            <p>{{ reviewEl.text }}</p>
+
+          </li>
+
+        </ul>
+
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
