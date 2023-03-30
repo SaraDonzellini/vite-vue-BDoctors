6ad2722043ecc2611bba4859d911529f534f23f0
SaraDonzellini <saradonzie@gmail.com>
2023-03-30 15:33:06 +0200

    add links a github

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -18,7 +18,7 @@ export default {
 </script>
 
 <template>
-  <hr>
+  <hr class='mt-5'>
   <div class="container">
     <div class="row">
       <div class="col-3">
@@ -37,7 +37,13 @@ export default {
       </div>
       <div class="col-3">
         <img src="public\imgs\Doctor-logo.png" style="width:30px" alt=""><h4 class="d-inline">BDoctors</h4>
-        <p class="m-0 p-0">&#169; Boolean by Alessio, Manuel, Sara, Vincenzo, Giada</p>
+        <p class="m-0 p-0">&#169; Boolean by 
+          <a href="https://github.com/AlessioNapoletano">Alessio</a>, 
+          <a href="https://github.com/ManuelBravaccini">Manuel</a>, 
+          <a href="https://github.com/SaraDonzellini">Sara</a>, 
+          <a href="https://github.com/Enzo-belardo">Vincenzo</a>, 
+          <a href="https://github.com/GiadaGallitto">Giada</a>
+        </p>
         <div class="box">
           <i class="fa-brands fa-facebook fa-2x"></i><span> - </span>
           <i class="fa-brands fa-square-twitter fa-2x"></i><span> - </span>
@@ -58,5 +64,13 @@ export default {
 
 p{
   font-size: 0.8rem;
+}
+
+a{
+  text-decoration: none;
+  color: $secondary-color;
+  &:hover{
+    color: $secondary-variant-color
+  }
 }
 </style>
