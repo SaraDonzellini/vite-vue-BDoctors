7f6636dad82ba5afdd65ed449d52b442665f0918
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2023-03-31 18:15:11 +0200

    Fixed animation button in header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -24,21 +24,25 @@ export default {
 
       <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto align-middle">
+
           <li class="nav-link d-flex flex-column text-center align-self-center">
             <router-link :to="{ name: 'home' }" :class="this.$route.name == 'home' ? 'active' : ''">
               Home
             </router-link>
           </li>
+
           <li class="nav-link d-flex flex-column text-center align-self-center">
             <router-link :to="{ name: 'doctors' }" :class="this.$route.name == 'doctors' ? 'active' : ''">
               Dottori
             </router-link>
           </li>
+
           <li class="nav-link flex-column text-center me-3">
-            <a class="my-btn btn" href="http://127.0.0.1:8000/register">
+            <a class="my-btn" href="http://127.0.0.1:8000/register">
               Sei un dottore? Registrati!
             </a>
           </li>
+
         </ul>
       </div>
 
@@ -49,6 +53,22 @@ export default {
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
 @use '../styles/partials/variables' as *;
+
+a.my-btn {
+  text-decoration: none;
+  border-radius: 25px;
+  color: $secondary-text-color;
+  background-color: $secondary-color;
+  padding: .5rem 1rem;
+  transition: .25s;
+  border: 0px;
+
+  &:hover {
+    background-color: $secondary-variant-color;
+    color: $secondary-text-color;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+  }
+}
 
 li {
   list-style-type: none;
